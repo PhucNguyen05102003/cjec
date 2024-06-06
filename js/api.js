3e304e0d6adba21ad19cedf1a399f5e6c46f16e0
Phúc Nguyen05102003 <phanrac8@gmail.com>
2024-06-06 10:27:43 +0700

    first commit

---
+++ js/api.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $.ajax({
+        url: 'http://localhost:3000/data',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            // $('#rac_huuco').text(data[0].huu_co);
+            // $('#rac_taiche').text(data[0].tai_che);
+            // $('#rac_nilong').text(data[0].ni_long);
+            // $('#rac_chuaphanloai').text(data[0].chua_phanloai);
+        },
+        error: function (error) {
+            console.log('Error:', error);
+        }
+    });
+});
